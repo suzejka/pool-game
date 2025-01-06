a7538d34c556a73e800a6f35a0e609734bab31cf
suzejka <zuzanna.julia.stasiak@gmail.com>
2025-01-06 23:30:07 +0100

    changed to posgres

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-import sqlite3
+import psycopg2
 import os
 from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -10,21 +13,36 @@ UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'supersecretkey'
 
-# Tworzenie bazy danych
-DATABASE = 'database.db'
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
 
 def init_db():
-    with sqlite3.connect(DATABASE) as conn:
+    with psycopg2.connect(
+        host=DATABASE_HOST,
+        database=DATABASE_NAME,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD
+    ) as conn:
         cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            nickname TEXT NOT NULL,
-                            avatar TEXT)''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS games (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            player1 TEXT,
-                            player2 TEXT,
-                            winner TEXT)''')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                nickname TEXT NOT NULL,
+                avatar TEXT,
+                beer_count INTEGER
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS games (
+                id SERIAL PRIMARY KEY,
+                player1 TEXT,
+                player2 TEXT,
+                winner TEXT
+            )
+        ''')
         conn.commit()
 
 init_db()
@@ -34,15 +52,20 @@ init_db()
 def index():
     if 'nickname' in session:
         nickname = session['nickname']
-        with sqlite3.connect(DATABASE) as conn:
+        with psycopg2.connect( 
+            host=DATABASE_HOST,
+            database=DATABASE_NAME,
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD
+            ) as conn:
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM users WHERE nickname = ?', (nickname,))
+            cursor.execute('SELECT * FROM users WHERE nickname = %s', (nickname,))
             user = cursor.fetchone()
             if user:
                 cursor = conn.cursor()
-                cursor.execute('SELECT beer_count FROM users WHERE nickname = ?', (nickname,))
+                cursor.execute('SELECT beer_count FROM users WHERE nickname = %s', (nickname,))
                 beers = cursor.fetchone()
-                cursor.execute('SELECT COUNT(*) FROM games WHERE winner = ?', (nickname,))
+                cursor.execute('SELECT COUNT(*) FROM games WHERE winner = %s', (nickname,))
                 games = cursor.fetchone()
 
                 games_count = games[0] if games else 0
@@ -60,9 +83,14 @@ def login():
         nickname = request.form['nickname']
         
         # Sprawdzenie, czy użytkownik istnieje w bazie
-        with sqlite3.connect(DATABASE) as conn:
+        with psycopg2.connect( 
+            host=DATABASE_HOST,
+            database=DATABASE_NAME,
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD
+            ) as conn:
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM users WHERE nickname = ?', (nickname,))
+            cursor.execute('SELECT * FROM users WHERE nickname = %s', (nickname,))
             user = cursor.fetchone()
         
         # Jeśli użytkownik nie istnieje, przekierowanie do strony dodawania avatara
@@ -88,9 +116,14 @@ def add_avatar():
             avatar.save(avatar_path)
             
             # Zapisz avatar do bazy danych
-            with sqlite3.connect(DATABASE) as conn:
+            with psycopg2.connect( 
+                host=DATABASE_HOST,
+                database=DATABASE_NAME,
+                user=DATABASE_USER,
+                password=DATABASE_PASSWORD
+                ) as conn:
                 cursor = conn.cursor()
-                cursor.execute('INSERT INTO users (nickname, avatar) VALUES (?, ?)', 
+                cursor.execute('INSERT INTO users (nickname, avatar) VALUES (%s, %s)', 
                                (session['nickname'], avatar_path))
                 conn.commit()
             
@@ -121,10 +154,15 @@ def add_game():
                 winner = player2
 
         # Zapisz grę do bazy danych
-        with sqlite3.connect(DATABASE) as conn:
+        with psycopg2.connect( 
+            host=DATABASE_HOST,
+            database=DATABASE_NAME,
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD
+            ) as conn:
             cursor = conn.cursor()
             cursor.execute('''INSERT INTO games (player1, player2, winner) 
-                              VALUES (?, ?, ?)''', (player1, player2, winner))
+                              VALUES (%s, %s, %s)''', (player1, player2, winner))
             conn.commit()
 
         return redirect(url_for('index'))  # Przekierowanie na stronę główną lub inną stronę po dodaniu gry
@@ -135,9 +173,14 @@ def add_game():
 def add_beer():
     if 'nickname' in session:
         nickname = session['nickname']
-        with sqlite3.connect(DATABASE) as conn:
+        with psycopg2.connect( 
+            host=DATABASE_HOST,
+            database=DATABASE_NAME,
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD
+            ) as conn:
             cursor = conn.cursor()
-            cursor.execute('UPDATE users SET beer_count = beer_count + 1 WHERE nickname = ?', (nickname,))
+            cursor.execute('UPDATE users SET beer_count = beer_count + 1 WHERE nickname = %s', (nickname,))
             conn.commit()
         return redirect(url_for('index'))  # Po dodaniu piwa wróć do strony głównej
     return redirect(url_for('login'))
