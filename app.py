a33d5428d821170b55706c3bdde7a98f167014b0
suzejka <zuzanna.julia.stasiak@gmail.com>
2025-01-07 22:23:35 +0100

    removed avatar monit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import psycopg2
 import os
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -31,7 +32,7 @@ def init_db():
                 id SERIAL PRIMARY KEY,
                 nickname TEXT NOT NULL,
                 avatar TEXT,
-                beer_count INTEGER
+                beer_count INTEGER DEFAULT 0
             )
         ''')
 
@@ -40,7 +41,9 @@ def init_db():
                 id SERIAL PRIMARY KEY,
                 player1 TEXT,
                 player2 TEXT,
-                winner TEXT
+                winner TEXT,
+                game_type TEXT,
+                date TIMESTAMP DEFAULT NOW()
             )
         ''')
         conn.commit()
@@ -72,8 +75,7 @@ def index():
                 beers_count = beers[0] if beers else 0
 
                 return render_template('index.html', user=nickname, avatar=user[2], games=games_count, beers=beers_count)  # Avatar z bazy danych
-            else:
-                return redirect(url_for('add_avatar'))  # Jeśli brak użytkownika w bazie
+             # Jeśli brak użytkownika w bazie
     return redirect(url_for('login'))  # Jeśli nie ma zalogowanego użytkownika
 
 # Strona logowania
@@ -98,42 +100,106 @@ def login():
             session['nickname'] = nickname
             return redirect(url_for('index'))
         else:
+            with psycopg2.connect( 
+            host=DATABASE_HOST,
+            database=DATABASE_NAME,
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD
+            ) as conn:
+                cursor = conn.cursor()
+                cursor.execute('INSERT INTO users (nickname, avatar) VALUES (%s, %s)', 
+                               (session['nickname'], "path"))
+        
             session['nickname'] = nickname
-            return redirect(url_for('add_avatar'))
+            return redirect(url_for('index'))
     
     return render_template('login.html')
 
-# Strona dodawania avatara
-@app.route('/add_avatar', methods=['GET', 'POST'])
-def add_avatar():
-    if request.method == 'POST':
-        avatar = request.files.get('avatar')
-        
-        if avatar:
-            # Zapisz avatar w folderze
-            filename = secure_filename(avatar.filename)
-            avatar_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            avatar.save(avatar_path)
-            
-            # Zapisz avatar do bazy danych
-            with psycopg2.connect( 
-                host=DATABASE_HOST,
-                database=DATABASE_NAME,
-                user=DATABASE_USER,
-                password=DATABASE_PASSWORD
-                ) as conn:
-                cursor = conn.cursor()
-                cursor.execute('INSERT INTO users (nickname, avatar) VALUES (%s, %s)', 
-                               (session['nickname'], avatar_path))
-                conn.commit()
-            
-            return redirect(url_for('index'))
-        else:
-            flash("Proszę wybrać avatar!", "error")
-    
-    return render_template('add_avatar.html')
+from flask import session, redirect, url_for
 
-# Strona dodawania gry
+@app.route('/logout')
+def logout():
+    session.clear()  # Wyczyść sesję użytkownika
+    return redirect(url_for('login'))  # Przekieruj na stronę logowania
+
+@app.route('/your_games')
+def your_games():
+    today = datetime.now().date()
+    seven_days_ago = today - timedelta(days=7)
+    
+    # Odczytaj nickname z sesji
+    nickname = session.get('nickname')  # Załóżmy, że nickname jest zapisany w sesji
+
+    # Jeśli brak nickname, użytkownik nie jest zalogowany, przekieruj
+    if not nickname:
+        return redirect(url_for('login'))  # Możesz przekierować do strony logowania
+
+    with psycopg2.connect(
+        host=DATABASE_HOST,
+        database=DATABASE_NAME,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD
+    ) as conn:
+        cursor = conn.cursor()
+        
+        # Gry z dzisiaj, gdzie nickname pasuje do gracza
+        cursor.execute('''
+            SELECT player1, player2, winner, game_type, date::date AS game_date
+            FROM games 
+            WHERE (player1 = %s OR player2 = %s) AND date::date = %s
+            ORDER BY date DESC
+        ''', (nickname, nickname, today))
+        today_games = cursor.fetchall()
+        
+        # Gry z ostatnich 7 dni, gdzie nickname pasuje do gracza
+        cursor.execute('''
+            SELECT player1, player2, winner, game_type, date::date AS game_date
+            FROM games 
+            WHERE (player1 = %s OR player2 = %s) AND date::date BETWEEN %s AND %s
+            AND date::date != %s
+            ORDER BY date DESC
+        ''', (nickname, nickname, seven_days_ago, today, today))
+        past_week_games = cursor.fetchall()
+    
+    return render_template(
+        'your_games.html',
+        today_games=today_games,
+        past_week_games=past_week_games
+    )
+
+@app.route('/all_games')
+def all_games():
+    # Logika wyświetlania wszystkich gier
+    return "Wszystkie gry"
+
+# # Strona dodawania avatara
+# @app.route('/add_avatar', methods=['GET', 'POST'])
+# def add_avatar():
+#     if request.method == 'POST':
+#         avatar = request.files.get('avatar')
+        
+#         if avatar:
+#             # Zapisz avatar w folderze
+#             filename = secure_filename(avatar.filename)
+#             avatar_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#             avatar.save(avatar_path)
+            
+#             # Zapisz avatar do bazy danych
+#             with psycopg2.connect( 
+#                 host=DATABASE_HOST,
+#                 database=DATABASE_NAME,
+#                 user=DATABASE_USER,
+#                 password=DATABASE_PASSWORD
+#                 ) as conn:
+#                 cursor = conn.cursor()
+#                 cursor.execute('INSERT INTO users (nickname, avatar) VALUES (%s, %s)', 
+#                                (session['nickname'], avatar_path))
+#                 conn.commit()
+            
+#         return redirect(url_for('index'))
+        
+#     return render_template('add_avatar.html')
+
 @app.route('/add_game', methods=['GET', 'POST'])
 def add_game():
     if request.method == 'POST':
@@ -143,7 +209,7 @@ def add_game():
         # Sprawdzamy, który checkbox jest zaznaczony i zapisujemy zwycięzcę
         winner = request.form.get('winner')
 
-        # Jeśli żaden checkbox nie jest zaznaczony, to ustawiamy winner jako 'brak'
+        # Jeśli żaden checkbox nie jest zaznaczony, ustawiamy zwycięzcę jako "Remis"
         if not winner:
             winner = "Remis"
         else:
@@ -153,19 +219,30 @@ def add_game():
             elif winner == 'player2':
                 winner = player2
 
+        # Pobieramy typ gry (8-ball lub 9-ball)
+        game_type = request.form.get('game_type')
+
+        # Upewniamy się, że game_type jest ustawiony
+        if game_type not in ['8-ball', '9-ball']:
+            flash("Wybierz poprawny typ gry (8-ball lub 9-ball).", "error")
+            return redirect(url_for('add_game'))
+
         # Zapisz grę do bazy danych
-        with psycopg2.connect( 
+        with psycopg2.connect(
             host=DATABASE_HOST,
             database=DATABASE_NAME,
             user=DATABASE_USER,
             password=DATABASE_PASSWORD
-            ) as conn:
+        ) as conn:
             cursor = conn.cursor()
-            cursor.execute('''INSERT INTO games (player1, player2, winner) 
-                              VALUES (%s, %s, %s)''', (player1, player2, winner))
+            cursor.execute('''
+                INSERT INTO games (player1, player2, winner, game_type) 
+                VALUES (%s, %s, %s, %s)
+            ''', (player1, player2, winner, game_type))
             conn.commit()
 
-        return redirect(url_for('index'))  # Przekierowanie na stronę główną lub inną stronę po dodaniu gry
+        flash("Gra została dodana pomyślnie!", "success")
+        return redirect(url_for('index'))  # Przekierowanie na stronę główną
 
     return render_template('add_game.html')
 
